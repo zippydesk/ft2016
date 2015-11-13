70781c893f875666dd19b850fb45cbb71d4cecef
Paweł Czerski <pawel.czerski@gmail.com>
2015-11-14 00:53:41 +0100

    Create id-talks.js

---
+++ id-talks.js
@@ -0,0 +1,8 @@
+---
+---
+
+var ID_TALKS = {
+	{% for talk in site.data.talks %}
+		'{{ talks[1].id | escape }}':'{{ talk[1].title | escape }}&emsp;{{ talk[1].id | escape }}'{% unless forloop.last %},{% endunless %}
+	{% endfor %}
+};
