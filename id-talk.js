a0ea214601b4fc276e363e21216f0496da7c57c0
Paweł Czerski <pawel.czerski@gmail.com>
2015-11-14 00:55:29 +0100

    Update and rename id-talks.js to id-talk.js

--- id-talk.js
+++ id-talk.js
@@ -1,7 +1,7 @@
 ---
 ---
 
-var ID_TALKS = {
+var ID_TALK = {
 	{% for talk in site.data.talks %}
 		'{{ talks[1].id | escape }}':'{{ talk[1].title | escape }}&emsp;{{ talk[1].id | escape }}'{% unless forloop.last %},{% endunless %}
 	{% endfor %}
