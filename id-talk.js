d640e3eaa177e37765e2385d04734e44be685fcc
Paweł Czerski <pawel.czerski@gmail.com>
2015-11-14 01:02:34 +0100

    Delete id-talk.js

--- id-talk.js
+++
@@ -1,8 +0,0 @@
----
----
-
-var ID_TALK = {
-	{% for talk in site.data.talks %}
-		'{{ talks[1].id | escape }}':'{{ talk[1].title | escape }}&emsp;{{ talk[1].id | escape }}'{% unless forloop.last %},{% endunless %}
-	{% endfor %}
-};
